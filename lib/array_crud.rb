a2ac2b17804581904dab1c89042720c66f3bd61b
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-17 01:30:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["Stark", "Lannister", "Tully", "Baratheon"]
-  element = "Targaryan"
+  element = "wow"
   array.unshift element
 end
 
