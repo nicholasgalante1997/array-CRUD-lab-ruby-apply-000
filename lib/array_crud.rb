15794c33a924c68086a33b04b8823ae07f276b01
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-17 01:26:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Arya", "Bran", "Sansa", "Robb"]
-  array.push "Jon Snow"
+  element = "Jon Snow"
+  element << array
 end
 
 def add_element_to_start_of_array(array, element)
